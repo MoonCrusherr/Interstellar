ed2b6b15b76856238272e58009a7c4291a479eeb
MoonCrusherr <143880505+MoonCrusherr@users.noreply.github.com>
2025-11-11 17:47:30 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    moon: "2turntttt",
+    moon: "richoffewh",
   },
 };
 
